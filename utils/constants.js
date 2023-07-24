2cf189ff155018b37056f157683e4986bca3317f
Maruf <marufdev489@gmail.com>
2023-07-24 13:02:58 +0600

    delivery, privacy, t&c, refund Added!

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,11 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Next JS';
 
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://127.0.0.1:3000';
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://127.0.0.1:8000';
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://13.213.162.196/ecom';
 
-export const STORAGE_URL = process.env.NEXT_PUBLIC_STORAGE_URL || 'http://127.0.0.1:8000';
+export const STORAGE_URL = process.env.NEXT_PUBLIC_STORAGE_URL || 'http://13.213.162.196';
+
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://13.213.162.196';
+
+export const IMAGE_STORAGE_URL = process.env.NEXT_PUBLIC_IMAGE_STORAGE_URL || 'http://192.168.10.30:4444/storage';
