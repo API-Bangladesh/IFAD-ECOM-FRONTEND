26a121e09e65b880a66aaefdddcba2aeb50cee0a
Md. Masudul Kabir <masud.ncse@gmail.com>
2023-07-26 21:30:33 +0600

    b2b updated order data fetch done

--- components/profile/Orders.js
+++ components/profile/Orders.js
@@ -2,24 +2,23 @@ import Row from "react-bootstrap/Row";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {BACKEND_URL} from "../../utils/constants";
+import {fetchOrders} from "../../services/OrderServices";
 
 const Orders = () => {
-    const [myOrder, setMyOrder] = useState([]);
+    const [orders, setOrders] = useState([]);
 
-    const fetchData = async () => {
-        try {
-            await axios.get(`${BACKEND_URL}/orders`)
-                .then((res) => {
-                    setMyOrder(res.data.data);
-                    console.log(myOrder);
-                })
-        } catch (err) {
-            console.log(err.message);
-        }
-    };
+    const fetchOrdersData = async () => {
+        fetchOrders({
+            paginate: 'yes'
+        }).then((response) => {
+            if (response?.data?.data) {
+                setOrders(response.data.data);
+            }
+        });
+    }
 
     useEffect(() => {
-        fetchData();
+        fetchOrdersData();
     }, []);
 
     return (
@@ -39,50 +38,18 @@ const Orders = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            {
-                                myOrder.map((item) => {
-                                    <>
-                                        {/* <th scope="row" className="">
-                    <Image src={OrderImg} alt="" className="order-item-one" />
-                  </th> */}
-                                        <td className="order-list mt-3 text-capitalize">{item.id}</td>
-                                        <td>{item.order_date}</td>
-                                        <td>{item.payment_status_id}</td>
-                                        <td>{item.order_status_id}</td>
-                                        <td>{item.grand_total}</td>
-                                        {/* <td>5000Tk</td> */}
-                                        {/* <td className="text-center">
-                    <Button variant="success" className="order-icon-bg rounded-1 px-2 bg-success">
-                      <BiDollar />
-                    </Button>
-                  </td> */}
-                                        {/* <td>
-                    <div className="d-flex justify-content-start order-icons">
-                      <Link href="/payments/Payments">
-                        <Button variant="success" className="order-icon-bg rounded-1 px-2 bg-success me-2">
-                          <svg width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
-                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                          </svg>
-                        </Button>
-                      </Link>
-                      <div>
-                        <Button variant="danger" className="order-icon-bg rounded-1 px-2 bg-danger">
-                          <RxCross2 />
-                        </Button>
-                      </div>
-                    </div>
-  
-                  </td> */}
-                                    </>
-                                })
-                            }
-                        </tr>
+                        {orders.map((item, key) => (
+                            <tr>
+                                <td className="order-list mt-3 text-capitalize">{item.id}</td>
+                                <td>{item.order_date}</td>
+                                <td>{item.payment_status_id}</td>
+                                <td>{item.order_status_id}</td>
+                                <td>{item.grand_total}</td>
+                            </tr>
+                        ))}
                         </tbody>
                     </table>
                 </div>
-
             </Row>
         </>
     )
