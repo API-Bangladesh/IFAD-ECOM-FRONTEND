00eade5d9df493145c5f441fa877dd5e26ba0eff
nazmul <nazmulhussain.api@gmail.com>
2023-08-19 21:53:12 +0600

    Update invoice design

--- components/common/InvoiceCard.js
+++ components/common/InvoiceCard.js
@@ -21,7 +21,7 @@ const InvoiceCard = ({ orderDetails }) => {
   };
 
   return (
-    <div className="card">
+    <div className="">
       <div className="card-body">
         <div className="container mb-5 mt-3">
           <div className="row d-flex align-items-baseline">
@@ -45,15 +45,17 @@ const InvoiceCard = ({ orderDetails }) => {
               </div>
             </div> */}
             {orderDetails?.order_items?.map((item, i) => (
-              <div className="row my-2 mx-2 d-flex justify-content-between">
+              <div className="row my-2 d-flex justify-content-between">
                 <div key={i} className="col-md-7 mb-4 mb-md-0">
-                  <p className="">{item?.inventory?.product?.name}</p>
+                  {
+                    item?.type == "product" ? <p className="">{item?.inventory?.title}</p> : <p className="">{item?.combo?.title}</p>
+                  }
                 </div>
                 <div className="col-md-3 mb-4 mb-md-0">
                   <h5 className="mb-2 text-right">
                     {/* <s className="text-muted me-2 small align-middle"></s> */}
                     <span className="align-middle">
-                      Price: {item?.unit_price} bdt
+                      Price: {item?.unit_price} BDT
                     </span>
                   </h5>
                 </div>
@@ -64,17 +66,17 @@ const InvoiceCard = ({ orderDetails }) => {
             <div className="mb-4 mb-md-0">
               <h5 className="mb-2 d-flex justify-content-end">
                 <s className="text-muted me-2 small "></s>
-                <span className="float-end pt-2">
-                  Sub-Total {orderDetails?.sub_total} bdt
+                <span className="float-start pt-2 mx-4">
+                  Sub-Total {orderDetails?.sub_total} BDT
                 </span>
               </h5>
             </div>
             <hr />
           </div>
-          <div className="d-flex justify-content-end">
+          <div className="d-flex justify-content-end mx-3">
             {orderDetails.payment_status_id == 1 ?
             <button
-              className="btn btn-outline-warning rounded-1 py-1 disabled"
+              className="btn btn-outline-warning rounded-1 py-1 disabled "
             >
               Paid
             </button> :
@@ -87,19 +89,19 @@ const InvoiceCard = ({ orderDetails }) => {
             </button>}
           </div>
 
-          <div className="container">
             <div className="row mb-3">
               <p className="fw-bold mb-2 mt-2">Order Summary</p>
               <div className="col-xl-6">
-                <div className="payment-info">
+                <div className="payment-info my-2">
                   <h4 className="fw-bold">Payment Information</h4>
                   <p className="payment">
                     {orderDetails?.payment_method?.code}
                   </p>
                   <p className="desc">{orderDetails?.payment_method?.name}</p>
                 </div>
+                <hr/>
                 <div className="handle-info">
-                  <h4 className="fw-bold">Shipping & Handling Information</h4>
+                  <h4 className="fw-bold mt-2">Shipping & Handling Information</h4>
                   <p className="delivery-info">
                     Regular Delivery in 3-5 working Days.
                   </p>
@@ -110,20 +112,20 @@ const InvoiceCard = ({ orderDetails }) => {
                 <ul className="list-unstyled">
                   <li className="text-muted ms-3 d-flex justify-content-between">
                     <span className="text-black me-4">SubTotal</span>{" "}
-                    <span>{orderDetails?.sub_total} bdt</span>
+                    <span>{orderDetails?.sub_total} BDT</span>
                   </li>
                   <li className="text-muted ms-3 d-flex justify-content-between">
                     <span className="text-black me-4">Shipping & Handling</span>{" "}
-                    <span>{orderDetails?.shipping_charge} bdt</span>
+                    <span>{orderDetails?.shipping_charge} BDT</span>
                   </li>
                   <li className="text-muted ms-3 d-flex justify-content-between">
                     <span className="text-black me-4">Discount</span>{" "}
-                    <span>{orderDetails?.discount} bdt</span>
+                    <span>{orderDetails?.discount} BDT</span>
                   </li>
                   <hr />
                   <li className="text-muted ms-3 d-flex justify-content-between">
                     <span className="text-black me-4">Grand Total</span>{" "}
-                    <span>{orderDetails?.grand_total} bdt</span>
+                    <span>{orderDetails?.grand_total} BDT</span>
                   </li>
                 </ul>
               </div>
@@ -133,15 +135,15 @@ const InvoiceCard = ({ orderDetails }) => {
 
             <div className="row mt-3">
               <p className="fw-bold pb-2">Address</p>
-              <div className="col-xl-6">
-                <p className="ms-3 fw-bold">Billing Address</p>
+              <div className="col-xl-5 bg-light p-2 mr-20">
+                <p className="ms-3 fw-bold ">Billing Address</p>
                 {billing_lines?.map((line, i) => (
                   <p key={i} className="ms-3">
                     {line}
                   </p>
                 ))}
               </div>
-              <div className="col-xl-6">
+              <div className="col-xl-5 bg-light p-2">
                 <p className="ms-3 fw-bold">Shipping Address</p>
                 {shipping_lines?.map((line, i) => (
                   <p key={i} className="ms-3">
@@ -150,7 +152,6 @@ const InvoiceCard = ({ orderDetails }) => {
                 ))}
               </div>
             </div>
-          </div>
         </div>
       </div>
     </div>
