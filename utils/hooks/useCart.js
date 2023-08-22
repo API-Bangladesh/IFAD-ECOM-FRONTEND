21129ab969a8cb70e08e04fa74b50ce56155b4e6
Nazmul Hussain <nazmulhussain.api@gmail.com>
2023-08-22 14:04:42 +0600

    Remove combo weight

--- utils/hooks/useCart.js
+++ utils/hooks/useCart.js
@@ -81,9 +81,11 @@ export const useCart = (initialValue) => {
         item.items.forEach((i) => {
           // console.log(i);
           const obj = {
-            variant_quantity:
-              i.inventory_variants?.[0]?.variant_option?.name || "0gm",
-            quantity: (i?.quantity || 1) * (item?.quantity || 1),
+            // variant_quantity:
+            //   i.inventory_variants?.[0]?.variant_option?.name || "0gm",
+            // quantity: (i?.quantity || 1) * (item?.quantity || 1),
+            variant_quantity: "500gm",
+            quantity: 1,
           };
           items.push(obj);
         });
